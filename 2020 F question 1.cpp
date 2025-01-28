07c8141114a50091e03b694c7dccb109c546fd5b
Pamela2288 <sbhekokuhlepamela@gmail.com>
2025-01-28 08:36:46 +0200

    Add files via upload

---
+++ 2020 F question 1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name ;
+    string surname ;
+    string program ;
+    int age;
+    int users [10];
+    for (int i=0;i<=10;i++){
+    cout << "enter name" << endl;
+    cin >> name;
+    cout << "enter surname" << endl;
+    cin >> surname;
+    cout << "enter age" << endl;
+    cin >> age;
+    cout << "program" << endl;
+    cin >> program;
+    }
+    return 0;
+}
